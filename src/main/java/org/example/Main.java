aff7201ed4afd28ca184dc1fd6d4d8a23c1bb319
JorgeMataH <matahj@gmail.com>
2022-12-10 13:05:55 -0600

    Se hace un programa para cambio de base en el Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int number;
+
+        do{
+            System.out.println("Escribe un número para transformarlo en binario (0 para terminar):");
+            number = sc.nextInt();
+            String binary = Integer.toBinaryString(number);
+            System.out.println("(" + number +") en base 10 = (" + binary + ") en base 2");
+        }while (number > 0);
     }
 }
